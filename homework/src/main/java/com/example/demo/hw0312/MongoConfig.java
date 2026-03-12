1fd8357fc72543bab0271a2b9c1c23d76eab42d2
lgnorant-lu <151433668+lgnorant-lu@users.noreply.github.com>
2026-03-12 11:30:30 +0000

    feat(hw): 处理芒狗配置读入问题.

---
+++ homework/src/main/java/com/example/demo/hw0312/MongoConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.hw0312;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database:chatdb}")
+    private String databaseName;
+
+    @Bean
+    public MongoClient explicitMongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(MongoClient explicitMongoClient) {
+        return new SimpleMongoClientDatabaseFactory(explicitMongoClient, databaseName);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTemplate(mongoDatabaseFactory);
+    }
+}
